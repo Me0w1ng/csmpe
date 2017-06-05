dbbe508b9827d97eed8fa424151d239391e3b497
Alex Tang <alextang@cisco.com>
2017-06-05 15:48:06 -0700

    Add IOSXRv-X64 support

--- csmpe/core_plugins/csm_custom_commands_capture/plugin.py
+++ csmpe/core_plugins/csm_custom_commands_capture/plugin.py
@@ -33,7 +33,7 @@ from condoor.exceptions import CommandSyntaxError
 class Plugin(CSMPlugin):
     """This plugin captures custom commands and stores in the log directory."""
     name = "Custom Commands Capture Plugin"
-    platforms = {'ASR9K', 'CRS', 'NCS1K', 'NCS4K', 'NCS5K', 'NCS5500', 'NCS6K', 'ASR900', 'N6K', 'IOS-XRv'}
+    platforms = {'ASR9K', 'CRS', 'NCS1K', 'NCS4K', 'NCS5K', 'NCS5500', 'NCS6K', 'ASR900', 'N6K', 'IOSXRv-9K', 'IOSXRv-X64'}
     phases = {'Pre-Upgrade', 'Post-Upgrade', 'Migration-Audit', 'Pre-Migrate', 'Migrate', 'Post-Migrate'}
 
     def run(self):
--- csmpe/core_plugins/csm_install_operations/exr/deactivate.py
+++ csmpe/core_plugins/csm_install_operations/exr/deactivate.py
@@ -36,7 +36,7 @@ from csmpe.core_plugins.csm_install_operations.utils import get_cmd_for_install_
 class Plugin(CSMPlugin):
     """This plugin deactivates packages on the device."""
     name = "Install Deactivate Plugin"
-    platforms = {'ASR9K', 'NCS1K', 'NCS4K', 'NCS5K', 'NCS5500', 'NCS6K', 'IOS-XRv'}
+    platforms = {'ASR9K', 'NCS1K', 'NCS4K', 'NCS5K', 'NCS5500', 'NCS6K', 'IOSXRv-9K', 'IOSXRv-X64'}
     phases = {'Deactivate'}
     os = {'eXR'}
 
