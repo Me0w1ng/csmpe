498b78c56292684974276c70f6c6a47448bc0256
Klaudiusz Staniek <kstaniek@gmail.com>
2017-10-06 23:14:11 +0200

    Fix for missing install error details in the log

--- csmpe/core_plugins/csm_install_operations/exr/install.py
+++ csmpe/core_plugins/csm_install_operations/exr/install.py
@@ -32,7 +32,7 @@ from condoor import ConnectionError, CommandError, CommandSyntaxError
 from csmpe.core_plugins.csm_node_status_check.exr.plugin_lib import parse_show_platform
 from csmpe.core_plugins.csm_install_operations.actions import a_error
 
-install_error_pattern = re.compile("Error:    (.*)$", re.MULTILINE)
+install_error_pattern = re.compile("ERROR(.*)$", re.MULTILINE)
 
 plugin_ctx = None
 
